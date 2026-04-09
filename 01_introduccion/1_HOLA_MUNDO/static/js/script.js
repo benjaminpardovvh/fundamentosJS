a004833453cc192c2bd880c6f7c7713539199d3b
benjaminpardovvh <benjaminpardo@liceovvh.cl>
2026-04-09 12:59:55 -0400

    pusheando

---
+++ 01_introduccion/1_HOLA_MUNDO/static/js/script.js
@@ -0,0 +1,12 @@
+//prueba de conexion con js
+console.log("conexion exitosa con el archivo js");
+//1)hacer un hola mundo con variable y const.
+const saludo = "hola a todos, ";
+let destinatario = "estudiantes";
+console.log(saludo + "para los " + destinatario);
+
+// 2) saludar a una persona ingresada por teclado
+let nombre = prompt ("Ingrese su nombre: ");
+//imprimir un nombre ingresado con un prompt
+console.log("hola " + nombre);
+
---
+++ 02_tipos_de_datos/1_tipos_textos/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("conexion con js exitosamente hecha :3")
+/*
+Dato: tipo texto (string)
+concentación de texto con el signo +
+podemos unir texto y varialbles
+*/
+const nombre = "Johnny";
+const apellido = "Joestar";
+//unimos ambas constantes con un texto extra
+console.log("hola, soy " + nombre + " " + apellido + " y soy un ex-corredor de caballos")
