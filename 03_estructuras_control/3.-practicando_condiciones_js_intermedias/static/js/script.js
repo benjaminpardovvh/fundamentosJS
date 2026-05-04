e49c3706286211c9b213dcbeb8a4d77802761da9
benjaminpardovvh <benjaminpardo@liceovvh.cl>
2026-05-04 14:39:12 -0400

    actualización

--- 03_estructuras_control/3.-practicando_condiciones_js_intermedias/static/js/script.js
+++ 03_estructuras_control/3.-practicando_condiciones_js_intermedias/static/js/script.js
@@ -109,3 +109,123 @@ function ejercicio18() {
     else if (promedio >= 4.0) { alert(`El estudiante ${nombre3.toUpperCase()} (con ${nombre3.length} caracteres) tiene un promedio de: ${promedio} (Aprobado)`) }
     else { alert(`El estudiante ${nombre3.toUpperCase()} (con ${nombre3.length} caracteres) tiene un promedio de: ${promedio} (Reprobado)`) };
 };
+
+//Ejercicio 19: Clasificación de desempeño con más niveles
+//Crear una función que almacene el nombre de un estudiante y su promedio final. Luego:
+
+//Convertir el nombre a mayúsculas
+//Clasificar el rendimiento según:
+//≥ 6.5 → Sobresaliente --> if
+//≥ 6.0 → Muy buen rendimiento --> else-if
+//≥ 5.0 → Buen rendimiento --> else-if
+//≥ 4.0 → Suficiente --> else-if
+//< 4.0 → Insuficiente --> else
+
+//Además:
+//Mostrar la cantidad de caracteres del nombre
+
+function ejercicio19() {
+    let nombre4 = "Koichi Hirose";
+    let noteK = 5.6;
+    let noteO = 7.0;
+    let noteI = 5.4;
+    let promedio2 = (noteK + noteO + noteI) / 3
+    if (promedio2 >= 6.5) { alert(`El estudiante ${nombre4.toUpperCase()} (con ${nombre4.length} caracteres) tiene un promedio de: ${promedio2} (Sobresaliente)`) }
+    else if (promedio2 >= 6.0) { alert(`El estudiante ${nombre4.toUpperCase()} (con ${nombre4.length} caracteres) tiene un promedio de: ${promedio2} (Muy buen rendimiento)`) }
+    else if (promedio2 >= 5.0) { alert(`El estudiante ${nombre4.toUpperCase()} (con ${nombre4.length} caracteres) tiene un promedio de: ${promedio2} (Buen rendimiento)`) }
+    else if (promedio2 >= 4.0) { alert(`El estudiante ${nombre4.toUpperCase()} (con ${nombre4.length} caracteres) tiene un promedio de: ${promedio2} (Suficiente)`) }
+    else { alert(`El estudiante ${nombre4.toUpperCase()} (con ${nombre4.length} caracteres) tiene un promedio de: ${promedio2} (Insuficiente)`) };
+};
+
+// Ejercicio 20: Clasificación de producto con múltiples rangos de precio
+// Crear una función que almacene el nombre de un producto y su precio. Luego:
+
+// Convertir el producto a minúsculas
+// Clasificar el precio:
+// ≥ 200.000 → Producto de lujo
+// ≥ 100.000 → Producto premium
+// ≥ 50.000 → Producto estándar
+// ≥ 20.000 → Producto económico
+// < 20.000 → Producto básico
+
+// Además:
+
+// Aplicar descuento:
+// Si es ≥ 100.000 → 15%
+// Si es ≥ 50.000 → 10%
+// Si es menor → sin descuento
+
+// Mostrar:
+
+// Nombre transformado
+// Largo del nombre del producto
+// Precio original
+// Precio final
+// Clasificación
+
+function ejercicio20() {
+    let dinero = 300000
+    let objeto = "Bate de baseball"
+    let dess1 = dinero * 0.15
+    let dess2 = dinero * 0.1
+    if (dinero >= 200000) {
+        alert(`El producto ${objeto.toLowerCase()} con ${objeto.length} caracteres, tiene un precio de: $${dinero} 
+    \nse le aplicara un 15% de descuento, el producto costara en total: $${dinero - dess1} (Producto de Lujo)`)
+    }
+    else if (dinero >= 100000) {
+        alert(`El producto ${objeto.toLowerCase()} con ${objeto.length} caracteres, tiene un precio de: $${dinero} 
+    \nse le aplicara un 10% de descuento, el producto costara en total: $${dinero - dess2} (Producto Premium)`)
+    }
+    else if (dinero >= 50000) {
+        alert(`El producto ${objeto.toLowerCase()} con ${objeto.length} caracteres, tiene un precio de: ${dinero}
+    \nse le aplicara un 10% de descuento, el producto costara en total: $${dinero - dess2} (Producto Estandar)`)
+    }
+    else if (dinero >= 20000) {
+        alert(`El producto ${objeto.toLowerCase()} con ${objeto.length} caracteres, tiene un precio de: $${dinero}
+    \nno se le aplicara descuento (Producto Economico)`)
+    }
+    else {
+        alert(`El producto ${objeto} con ${objeto.length} caracteres, tiene un precio de: ${dinero}
+    \nno se le aplicara descuento (Producto Basico)`)
+    };
+};
+
+// Ejercicio 21: Evaluación de frase y puntaje con múltiples condiciones
+// Crear una función que almacene una frase y un puntaje. Luego:
+
+// Convertir la frase a mayúsculas
+// Contar su largo
+// Clasificar el puntaje:
+// ≥ 90 → Excelente
+// ≥ 80 → Muy bueno
+// ≥ 70 → Bueno
+// ≥ 60 → Regular
+// < 60 → Deficiente
+
+// Además:
+
+// Clasificar la frase por tamaño:
+// 30 → Muy larga
+// 20 → Larga
+// 10 → Mediana
+// ≤ 10 → Corta
+
+function ejercicio21() {
+    let homeless = "Por favor Velocidad, necesito esto, mi madre esta un poco sin hogar, vivo con mi padre y quiero ayudarla";
+    if (homeless.length >= 90) { alert(`La frase "${homeless.toUpperCase()}"
+con ${homeless.length} caracteres, tiene un puntaje Excelente y es una frase muy larga.`) }
+    else if (homeless.length >= 80) { alert(`La frase "${homeless.toUpperCase()}"
+con ${homeless.length} caracteres, tiene un puntaje Muy bueno y es una frase muy larga.`) }
+    else if (homeless.length >= 70) { alert(`La frase "${homeless.toUpperCase()}"
+con ${homeless.length} caracteres, tiene un puntaje bueno y es una frase muy larga.`) }
+    else if (homeless.length >= 60) { alert(`La frase "${homeless.toUpperCase()}"
+con ${homeless.length} caracteres,Tiene un puntaje regular y es una frase muy larga.`) }
+    else if (homeless.length < 60 && homeless.length >= 30) { alert(`La frase "${homeless.toUpperCase()}"
+con ${homeless.length} caracteres, tiene un puntaje deficiente y es una frase muy larga.`) }
+    else if (homeless.length < 60 && homeless.length >= 20) { alert(`La frase "${homeless.toUpperCase()}"
+con ${homeless.length} caracteres, tiene un puntaje deficiente y es una frase larga`) }
+    else if (homeless.length < 60 && homeless.length >= 10) { alert(`La frase "${homeless.toUpperCase()}"
+con ${homeless.length} caracteres, tiene un puntaje deficiente y es una frase mediana`) }
+    else { alert(`La frase "${homeless.toUpperCase()}"
+con ${homeless.length} caracteres, tiene un puntaje deficiente y es una frase corta`) }
+};
